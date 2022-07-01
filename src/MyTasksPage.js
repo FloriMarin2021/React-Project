b9728a4df0356668a7baf1b3985ff90e46a95c0e
FloriMarin2021 <florimarin2020@gmail.com>
2022-07-01 09:38:36 +0300

    ajutor map2

--- src/MyTasksPage.js
+++ src/MyTasksPage.js
@@ -3,19 +3,77 @@ import NavigationMeniu from './NavigationMeniu/NavigationMeniu';
 import Header from './Header/Header';
 import TaskForm from './TaskForm/TaskForm';
 import TaskTable from './TaskTable';
-import Table from './Table';
 
 
 class MyTasksPage extends React.Component{ 
+   constructor(props) {
+      super(props);
+     
+  
+      this.state = {
+        tasks: [
+         {
+             nr: '1',
+             description:'task nr 1',
+             date:'22.12.2022',
+             status:[ { id: 'open', label:'open' },
+                      { id: 'in progress', label:'in progress'},
+                      {  id: 'done', label:'done' }
+                    ],
+             action: ['o', 'e', 'd']
+           },     
+           {
+             nr: '2',
+             description:'task nr 2',
+             date:'10.05.2021',
+             status:[ { id:'open', label:'open' },
+                      { id: 'inprogress', label:'inprogress'},
+                      {  id: 'done', label:'done' }
+                    ],
+             action: ['o', 'e', 'd']
+           },    
+           {
+            nr: '3',
+             description:'task nr 3',
+             date:'14.03.2020',
+             status:[ { id: 'open', label:'open' },
+                      { id: 'in progress', label:'in progress'},
+                      {  id: 'done', label:'done' }
+                    ],
+             action: ['o', 'e', 'd']
+           }
+                 
+        ]
+      }
+    
+    }
+
+/*
+   
+  handleDeleted(id) {
+    const [list, setList] = React.useState(myTasks); 
+    const newList=list.filter((item)=>item.id!==id);
+          setList(newList); 
+
+        return (
+           <List list={list} onRemove={handleDeleted}/>
+            );
+    }
+handleDeleted={this.handleDeleted}
+*/
+
 
    render() {
+   
        return (
         <div className='MyTasksPage'>
+       
+           <TaskTable   tasks={this.state.tasks}/>  
            <NavigationMeniu />
            <Header />
-           <TaskForm />  
-           <TaskTable/>   
-           <Table/>                
+           <TaskForm/>
+            
+                          
         </div>   
        );
    }
--- src/TaskTable.js
+++ src/TaskTable.js
@@ -1,33 +1,128 @@
 import React from 'react';
+import MyTasksPage from './MyTasksPage';
 
-class TaskTable extends React.Component{ 
+function TaskTable({tasks}){ 
+
+     const taskList=tasks.map((task, index)=>{
+       return(
+         <div>
+           {task.map((item, i)=>{
+                  return <div></div>
+           })}
+           <div>{task.nr}</div>
+       
+       </div>
+      ) 
+      
+     })   
+    
+    return (     
+      <div>
+        <table className='table'>
+              <thead>
+                  <tr>
+                      <th>Id</th>
+                      <th>Description</th>
+                      <th>Date</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                  </tr>
+              </thead>
+              </table>
+        {taskList}      
+          </div>);
+          }  
+      
+          /*
+          <table className='table'>
+              <thead>
+                  <tr>
+                      <th>Id</th>
+                      <th>Description</th>
+                      <th>Date</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                  </tr>
+              </thead>
+              <tbody>
+           
+               
+                      <tr key={index}>
+                          <td>{task.description}</td>
+                          <td>{task.date}</td>
+                          <td>                                                            
+                              <select>                                   
+                                      <option>{task.status[0]}</option>
+                                      <option>{task.status[1]}</option>
+                                      <option>{task.status[2]}</option>                                    
+                              </select>                                                                 
+                          </td>
+                          <td >                                   
+                              <button>{task.action[0]}</button>
+                              <button>{task.action[1]}</button>
+                              <button >{task.action[2]}</button>                                     
+                          </td>                                                              
+                      </tr>
+                
+              </tbody>
+          </table>
+          
+         
+           ))}
+           */
      
+
+  
+
+
+ /*
+  updateMessage(event) {
+    this.setState({
+     
+    });
+  }
+ 
+
+    this.setState({
+      items: items
+    });
+  }
+
+  renderRows() {
+    var context = this;
+
+    return  this.state.items.map(function(o, i) {
+              return (
+                <tr key={"item-" + i}>
+                  <td>
+                    <input
+                      type="text"
+                      value={o}
+                      
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={context.handleItemDeleted.bind(context, i)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            });
+  }
+
+  render(){       
+        return (
+          <div>
+           
+    );
+  }
+
+  /*</div>
     render(){       
-        var  myTasks=[
-            {
-              id: '1',
-              description:'task nr 1',
-              date:'22.12.2022',
-              status:['open', 'done', 'in progress'],
-              action: ['o', 'e', 'd']
-            }, 
-    
-            {
-              id: '2',
-              description:'task nr 2',
-              date:'10.05.2021',
-              status:['open', 'done', 'in progress'],
-              action: ['o', 'e', 'd']
-            },
-    
-            {
-              id: '3',
-              description:'task nr 3',
-              date:'14.03.2020',
-              status:['open', 'done', 'in progress'],
-              action: ['o', 'e', 'd']
-            }                
-          ];   
+          
         return (
             <div>
                 <table className='table'>
@@ -41,8 +136,8 @@ class TaskTable extends React.Component{
                         </tr>
                     </thead>
                     <tbody>
-                   
-                        {myTasks.map((task, index)=>(
+                 
+                        {tasks.map((task, index)=>(
                             <tr key={index}>
                                 <td>{task.id}</td>
                                 <td>{task.description}</td>
@@ -67,7 +162,6 @@ class TaskTable extends React.Component{
         );
     }
 }     
-   
-
+ */  
 
 export default TaskTable;
