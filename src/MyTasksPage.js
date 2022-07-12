7dda5df0f08cd27ab494fda49ced0c6c650dbc6e
FloriMarin2021 <florimarin2020@gmail.com>
2022-07-12 08:57:59 +0300

    adaugare linie tabel

--- src/MyTasksPage.js
+++ src/MyTasksPage.js
@@ -4,15 +4,11 @@ import Header from './Header/Header';
 //import TaskForm from './TaskForm/TaskForm';
 import TaskTable from './TaskTable/TaskTable';
 import './MyTasksPage.css';
-
 import Modal from './Modal/Modal';
-
-
 
 class MyTasksPage extends React.Component{ 
    constructor(props) {
-      super(props);     
-  
+      super(props); 
       this.state = {
         tasks: [
          {
@@ -47,19 +43,74 @@ class MyTasksPage extends React.Component{
            }
                  
         ] ,
+
         isModalVisible: false, 
         
-        openedTask:""
-      }
-    
-    } 
+        openedTask:"",
 
+        newRows:{           
+          nr:'',        
+          description:'',
+          date:'',
+          status:'' ,
+          action:'',
+            },
+
+        displayForm: false,
+
+        hideTabel:true,
+            
+        }   
+    }
+
+hideComponent= ()=> {
+      this.setState({
+        hideTabel: this.hideTabel       
+             });
+    }; 
+
+showRow= ()=> {
+      this.setState({
+        displayForm: !this.displayForm        
+             });
+    };
+ 
+addRow = () => {
+
+  this.setState(({ newRows, tasks }) => {
+    return {
+      tasks: [...tasks, { ...newRows, id: tasks.length + 1 }],
+      newRows: {           
+        nr:'',        
+        description:'',
+        date:'',
+        status:'' ,
+        action:'',
+          }         
+    }   
+  })     
+    }; 
+  
+
+handleChange= (event) => { 
+ 
+     const fieldName = event.target.getAttribute("name");
+     const fieldValue = event.target.value;     
+     const newRows={...this.newRows}
+     newRows[fieldName] = fieldValue; 
+
+         this.setState(
+          { newRows: { ...newRows, 
+           [fieldName]:fieldValue}})      
+  } 
+  
 showModal = (task)=> {
       this.setState({
         isModalVisible: !this.state.isModalVisible,
         openedTask: task,
-      });
+             });
     };  
+ 
          
 deleteRow=(idx)=>{ 
      this.setState({
@@ -73,6 +124,7 @@ deleteRow=(idx)=>{
    
        return (        
         <div className='my-task-page'>
+       
           <NavigationMeniu className='navigation-menu' />
           <Header className='header'/>       
           <TaskTable className='task-table' 
@@ -81,7 +133,15 @@ deleteRow=(idx)=>{
                      showModal={this.showModal}
                      isModalVisible={this.state.isModalVisible}
                      onClose={this.showModal}
-                     openedTask={this.state.openedTask} />
+                     openedTask={this.state.openedTask}                    
+                     newRows={this.state.newRows}
+                     addRow={this.addRow}
+                     handleChange={this.handleChange} 
+                     displayForm={this.state.displayForm}
+                     showRow={this.showRow} 
+                     hideTabel={this.state.hideTabel}     
+                     hideComponent={this.hideComponent}              
+                     />
           <Modal 
            openedTask={this.state.openedTask}
            onClose={this.showModal}           
--- src/TaskTable/TaskTable.js
+++ src/TaskTable/TaskTable.js
@@ -1,12 +1,21 @@
 import React from 'react';
 //import MyTaskPage  from './MyTasksPage';
-import './TaskTable.css';
+import './TaskTable.css';   
 
 
-function TaskTable({tasks, deleteRow,  showModal}){ 
-  return( 
-    <div className='modal' >  
-     <div className='task-table'>          
+
+function TaskTable({tasks, newRows, deleteRow, 
+      showModal, handleChange, addRow, showRow, displayForm, hideComponent, hideTabel}){ 
+ 
+  
+  return(  
+  <div>
+    <button  className='btn-hide' onClick={() => hideComponent(hideTabel)} >X</button> 
+
+       {hideTabel ? (
+        
+    <div className='modal' >    
+     <div className='task-table'>             
       <table className='task-table_table'>
         <thead>
             <tr className='task-table_table_title'>
@@ -17,12 +26,13 @@ function TaskTable({tasks, deleteRow,  showModal}){
                 <th className='task-table_table_title_action'>Action</th>
             </tr>
         </thead>           
-        <tbody className='task-table_table_body'>           
-            {tasks.map((task, index)=>{
+        <tbody className='task-table_table_body'
+         > 
+                    {tasks.map((task, index)=>{
               return (               
                 <tr key={task.index} className='task-table_table_body_content'>
                     <td className='task-table_table_content_nr'>{task.nr}</td>
-                    <td className='task-table_table_content_descr'>{task.description}</td>
+                                       <td className='task-table_table_content_descr'>{task.description}</td>
                     <td className='task-table_table_content_date'>{task.date}</td>
                     <td className='task-table_table_content_status'>                                                           
                         <select> 
@@ -37,21 +47,124 @@ function TaskTable({tasks, deleteRow,  showModal}){
                       <button
                            onClick={()=> {showModal(task);}}                                                    
                            className='task-table_table_content_actions_btn_o'>                            
-                      O</button>
+                      O</button> 
+                      {displayForm ? (
+             <div>
+                     <input 
+                       className='input_nr'
+                       type="text"
+                       name='nr'
+                       value={newRows.nr}                 
+                       onChange={handleChange}  
+                       /> 
+                    <input 
+                       className='task-table_table_content_descr'
+                       type="text"
+                       name='description'
+                       value={newRows.description}               
+                       onChange={handleChange}  
+                     />  
+                     <input 
+                       className='task-table_table_content_date' 
+                       type='text '
+                       name='date'
+                       value={newRows.date}                  
+                       onChange={handleChange}  
+                        />                                                                              
+                     <input
+                        className='task-table_table_content_status'
+                        type='text'
+                        name='status'
+                        value={newRows.status}
+                        onChange={handleChange}                    
+                        /> 
+                    <input
+                        className='task-table_table_content_status'
+                        type='text'
+                        name='action'
+                        value={newRows.action}
+                        onChange={handleChange}                        
+                        />                                                                                         
+                   <button
+                      type='submit'
+                      onClick={addRow}
+                      disabled={
+             !newRows.nr || !newRows.description ||!newRows.date ||!newRows.status||!newRows.action
+                                     }                  
+                    >create row</button>         
+        </div>
+         ) : (
+                      <button                       
+                             className='task-table_table_content_actions_btn_e' 
+                             onClick={() => showRow(!displayForm)}  >
+                             Edit</button>                  
+                         )}                 
+                           
                       <button
-                           className='task-table_table_content_actions_btn_e' >E</button>                
-                      <button
-                           className='task-table_table_content_actions_btn_d'
-                          
+                           className='task-table_table_content_actions_btn_d'                          
                            onClick={()=>deleteRow(index)}>D</button>                                                            
                     </td>                    
-                   </tr>              
-               ) })}
-        </tbody>                       
-    </table>
+                   </tr>                              
+              )  })}        
+          </tbody> 
+     </table> 
+     {displayForm ? (
+             <div>
+                     <input 
+                       className='input_nr'
+                       type="text"
+                       name='nr'
+                       value={newRows.nr}                 
+                       onChange={handleChange}                        
+                       /> 
+                     <input 
+                       className='task-table_table_content_descr'
+                       type="text"
+                       name='description'
+                       value={newRows.description}               
+                       onChange={handleChange} 
+                       />  
+                     <input 
+                       className='task-table_table_content_date' 
+                       type='text '
+                       name='date'
+                       value={newRows.date}                  
+                       onChange={handleChange} 
+                        />                                                                              
+                     <input
+                        className='task-table_table_content_status'
+                        type='text'
+                        name='status'
+                        value={newRows.status}
+                        onChange={handleChange}                      
+                        /> 
+                    <input
+                        className='task-table_table_content_status'
+                        type='text'
+                        name='action'
+                        value={newRows.action}
+                        onChange={handleChange}                       
+                        />                                                                                         
+                   <button
+                      type='submit'
+                      onClick={addRow} 
+                       disabled={
+     !newRows.nr ||!newRows.description||!newRows.date||!newRows.status||!newRows.action
+                    }                                   
+                    >Add </button>         
+        </div>
+         ) : (
+          <button onClick={() => showRow(!displayForm)} className="addRow">
+            Add Row 
+          </button>
+        )}
+     </div>     
    </div>
-   </div> 
-  );
+) : (
+         <div></div>
+)}
+  </div>
+  );    
  }
 
 export default TaskTable;
