13cedef6d9663d78c252f2f7ac67f3bcf2940895
FloriMarin2021 <florimarin2020@gmail.com>
2022-07-19 15:03:00 +0300

    librarie instalata si butoane incluse

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import NavigationMeniu from './NavigationMeniu/NavigationMeniu';
 
 
 function Home () {
     return  <div>  
-                   <h2>Home </h2>
-                   < NavigationMeniu/>                        
+              <h2>home</h2>  
+                 < NavigationMeniu/>                        
             </div>
 }
 export default Home;
