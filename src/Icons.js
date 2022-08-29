dc0d08641f53b54ee346ed6bf74f724e4d51e705
FloriMarin2021 <florimarin2020@gmail.com>
2022-08-29 11:13:22 +0300

    modificari icon

--- src/Icons.js
+++ src/Icons.js
@@ -3,46 +3,56 @@ import Icon from '@mdi/react'
 import { mdiAccount, mdiAccountSearch,  mdiAlarmCheck  } from '@mdi/js'
 import "./Icons.css";
 import NavigationMeniu from './NavigationMeniu/NavigationMeniu';
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect} from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 import Slide from "@mui/material/Slide";
 
 function Icons(){ 
   
-  const [showMessage, setShowMessage] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
   const [currentIcon, setCurrentIcon] = useState(null);
 
-  const [icons] =  useState([
-      {      
-     icon: "Account Search",
-      id: 0 
+  const icons=[
+      { 
+     icon:mdiAccountSearch,     
+     title: "Account Search",
+     id: 0,
+     color:"red"
        },       
-      {      
-        icon:"User Profile",
-        id:1             
+      {  
+        icon:mdiAccount,   
+        title:"User Profile",
+        id:1, 
+        color:"blue"            
       },
-     {      
-       icon: "Alarm Check",
-       id:2          
+     { 
+       icon: mdiAlarmCheck,     
+       title: "Alarm Check",
+       id:2, 
+       color:"green"          
       },      
-  ]);
-
-  const handleClick=(idx)=>{
-      // console.log("CURENTicons", currentIcon)
-     //  console.log("idx", idx)  
+    ];
+ 
+  const handleClick=(idx)=>{            
      setCurrentIcon(idx);  
 }
 
   const onClose = () => {   
     setShowMessage(false);
   };
-  
+
+  useEffect(() => {
+    if(currentIcon!==null){          
+    setShowMessage(true);
+  } 
+    
+}, [currentIcon]);
+ /* 
   const usePrevious = (currentIcon) => {    
-    const ref = useRef();
-  
+    const ref = useRef();  
      useEffect(() => {
         ref.current=currentIcon          
-        setShowMessage(true);  
+        setShowMessage(true); 
         
   }, [currentIcon]);
      return ref.current;
@@ -51,7 +61,7 @@ function Icons(){
   const prevIcon = usePrevious(currentIcon);
   //console.log("PREVICON",prevIcon)
   //console.log("currentICON", currentIcon)
-  
+ */ 
   return ( 
     <div className='icons'>        
             <div className='icons_nav_menu'>
@@ -60,35 +70,20 @@ function Icons(){
                  <div  className='icons_menu' >                      
                      {icons.map((item, index)=>(
                         <div key={index}             
-                         className={currentIcon === item.id ? "active" : ""} >
-       {item.id===0? <Icon path={mdiAccountSearch}
-                      title="Account Search"
-                      index={0}
-                      size={2} 
-                      horizontal  
-                      onClick={()=>handleClick(index)}                
-               />
-                 :null}
-       {item.id===1? <Icon path={mdiAccount}
-                      title="User Profile"
-                      size={2}
-                      horizontal      
-                      color="red"
-                      index={1}
-                      onClick={()=>handleClick(index)}
-                     />:null}
-       {item.id===2?  <Icon path={ mdiAlarmCheck }
-                    title="Alarm Check"
-                     size={2} 
-                     color="blue"
-                     horizontal
-                     index={2}
-                     onClick={()=>handleClick(index)}
-               />:null}           
-               </div>           
-                 ))}           
-            </div>
-            {(currentIcon===0||currentIcon===1||currentIcon===2)&&prevIcon!==null?
+                             className={currentIcon === item.id ? "active" : ""} >
+                         <Icon
+                          path={item.icon}
+                          title={item.title}
+                          color={item.color}
+                          index={index}
+                          size={2} 
+                          horizontal  
+                          onClick={()=>handleClick(index)}                
+                          />               
+                   </div>           
+                    ))}           
+                </div>
+
                  <Snackbar
                        anchorOrigin={{ vertical: "top", horizontal: "left" }}
                        key={`top,center`}
@@ -98,7 +93,7 @@ function Icons(){
                        autoHideDuration={3000}       
                        message={"icon changed"}
                        onClose={onClose}       
-                   /> :null }                          
+                   />                           
     </div>);
 }
 
